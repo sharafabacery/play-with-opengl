7e30b7b912d26ccee2aae03dc7d409c5f46f83ce
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2021-12-24 00:40:13 +0200

    into function

--- main.cpp
+++ main.cpp
@@ -26,121 +26,133 @@ static int stacks = 16;
 /* GLUT callback Handlers */
 GLint walkX=0,walkY=0;
 
-void Boat(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(0,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(2,1,slices,stacks);
-    glPopMatrix();
-}
-void Foot(double x,double y,double z){
- glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glutSolidCube(2);
-    glPopMatrix();
-
-}
-
-void Leg(double x,double y,double z,double a){
-        glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glRotated(a,0,0,1);
-        gluCylinder(gluNewQuadric(),.5,.5,7,slices,stacks);
-    glPopMatrix();
-
-}
-void Body(double x,double y,double z){
-glPushMatrix();
-        glColor3d(0,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glutSolidCube(10);
-
-    glPopMatrix();
-}
-
-void Nose(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glRotated(a,1,0,0);
-        glutSolidOctahedron();
-    glPopMatrix();
-}
-void Eye(double x,double y,double z,double a){
- glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glScalef(1.5,1.5,1.5);
-        glRotated(a,0,0,1);
-        glutSolidIcosahedron();
-    glPopMatrix();
-}
-void Mouth(double x,double y,double z,double a){
+void Boat(double x,double y,double z,double a)
+{
     glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,1,10);
-        glRotated(a,0,0,1);
-        glutSolidTorus(.25,1,slices,stacks);
+    glColor3d(0,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glRotated(a,0,0,1);
+    glutSolidTorus(2,1,slices,stacks);
     glPopMatrix();
 }
-void Cap(double x,double y,double z){
-glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(-1.25,8,-30);
-        glRotated(90,1,0,0);
-        glScalef(5.5,5,6);
-        glutSolidOctahedron();
-
-        glPopMatrix();
-}
-void ShoulderTemp(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,10,1);
-        glRotated(a,0,0,1);
-         gluCylinder(gluNewQuadric(),.5,.5,10,slices,stacks);
-    glPopMatrix();
-
-
-}
-void Shoulder(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(0,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glutSolidCube(3);
+void Foot(double x,double y,double z)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glutSolidCube(2);
     glPopMatrix();
 
 }
-void Arm(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(1,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glRotated(a,0,0,1);
-        gluCylinder(gluNewQuadric(),.5,.5,7,slices,stacks);
+
+void Leg(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glRotated(a,0,0,1);
+    gluCylinder(gluNewQuadric(),.5,.5,7,slices,stacks);
+    glPopMatrix();
+
+}
+void Body(double x,double y,double z)
+{
+    glPushMatrix();
+    glColor3d(0,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glutSolidCube(10);
+
     glPopMatrix();
 }
 
-void Hand(double x,double y,double z,double a){
-glPushMatrix();
-        glColor3d(0,0,0);
-        glTranslated(x,y,z);
-        glRotated(90,1,0,0);
-        glRotated(a,0,0,1);
-        glScalef(2,2,2);
-        glutSolidIcosahedron();
+void Nose(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glRotated(a,1,0,0);
+    glutSolidOctahedron();
+    glPopMatrix();
+}
+void Eye(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glScalef(1.5,1.5,1.5);
+    glRotated(a,0,0,1);
+    glutSolidIcosahedron();
+    glPopMatrix();
+}
+void Mouth(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,1,10);
+    glRotated(a,0,0,1);
+    glutSolidTorus(.25,1,slices,stacks);
+    glPopMatrix();
+}
+void Cap(double x,double y,double z)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(-1.25,8,-30);
+    glRotated(90,1,0,0);
+    glScalef(5.5,5,6);
+    glutSolidOctahedron();
+
+    glPopMatrix();
+}
+void ShoulderTemp(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,10,1);
+    glRotated(a,0,0,1);
+    gluCylinder(gluNewQuadric(),.5,.5,10,slices,stacks);
+    glPopMatrix();
+
+
+}
+void Shoulder(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(0,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glutSolidCube(3);
+    glPopMatrix();
+
+}
+void Arm(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(1,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glRotated(a,0,0,1);
+    gluCylinder(gluNewQuadric(),.5,.5,7,slices,stacks);
+    glPopMatrix();
+}
+
+void Hand(double x,double y,double z,double a)
+{
+    glPushMatrix();
+    glColor3d(0,0,0);
+    glTranslated(x,y,z);
+    glRotated(90,1,0,0);
+    glRotated(a,0,0,1);
+    glScalef(2,2,2);
+    glutSolidIcosahedron();
     glPopMatrix();
 
 }
@@ -167,49 +179,53 @@ static void display(void)
     glPushMatrix();
     glTranslatef(walkX,-1,walkY);
 
-        Boat(2.5,-10,-30,a);
-        Boat(-5.5,-10,-30,a);
+    Boat(2.5,-10,-30,a);
+    Boat(-5.5,-10,-30,a);
 
-        Foot(2.5,-7,-30);
-        Foot(-5.5,-7,-30);
+    Foot(2.5,-7,-30);
+    Foot(-5.5,-7,-30);
 
-        Leg(2.5,.5,-30,a);
-        Leg(-5.5,.5,-30,a);
+    Leg(2.5,.5,-30,a);
+    Leg(-5.5,.5,-30,a);
 
-        Body(-1.25,5,-30);
+    Body(-1.25,5,-30);
 
-        Nose(-1.25,5,-25,a);
+    Nose(-1.25,5,-25,a);
 
-        Eye(1,8,-25,a);
-        Eye(-3.5,8,-25,a);
+    Eye(1,8,-25,a);
+    Eye(-3.5,8,-25,a);
 
-        Mouth(-1.2,2,-24,a);
+    Mouth(-1.2,2,-24,a);
 
-        Cap(-1.25,8,-30);
+    Cap(-1.25,8,-30);
 
-        ShoulderTemp(-10,6,-28,a);
-        ShoulderTemp(-3,6,-28,a);
+    ShoulderTemp(-10,6,-28,a);
+    ShoulderTemp(-3,6,-28,a);
 
-        Shoulder(7.8,6,-28,a);
-        Shoulder(-10.8,6,-28,a);
+    Shoulder(7.8,6,-28,a);
+    Shoulder(-10.8,6,-28,a);
 
-        Arm(8,5,-28,a);
-        Arm(-11,5,-28,a);
+    Arm(8,5,-28,a);
+    Arm(-11,5,-28,a);
 
-        Hand(-11,-2,-28,a);
-        Hand(8,-2,-28,a);
+    Hand(-11,-2,-28,a);
+    Hand(8,-2,-28,a);
 
 
-glPopMatrix();
+    glPopMatrix();
 
     glutSwapBuffers();
 }
 void walk(int key,int x,int y)                                      //change positions using arrow keys
 {
-    if(key==GLUT_KEY_UP)    walkY+=1;
-    if(key==GLUT_KEY_DOWN)  walkY-=1;
-    if(key==GLUT_KEY_RIGHT) walkX+=1;
-    if(key==GLUT_KEY_LEFT)  walkX-=1;
+    if(key==GLUT_KEY_UP)
+        walkY+=1;
+    if(key==GLUT_KEY_DOWN)
+        walkY-=1;
+    if(key==GLUT_KEY_RIGHT)
+        walkX+=1;
+    if(key==GLUT_KEY_LEFT)
+        walkX-=1;
 
 }
 
@@ -217,23 +233,23 @@ static void key(unsigned char key, int x, int y)
 {
     switch (key)
     {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
+    case 27 :
+    case 'q':
+        exit(0);
+        break;
 
-        case '+':
-            slices++;
-            stacks++;
-            break;
+    case '+':
+        slices++;
+        stacks++;
+        break;
 
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+    case '-':
+        if (slices>3 && stacks>3)
+        {
+            slices--;
+            stacks--;
+        }
+        break;
     }
 
     glutPostRedisplay();
@@ -268,7 +284,7 @@ int main(int argc, char *argv[])
     glutReshapeFunc(resize);
     glutDisplayFunc(display);
     glutKeyboardFunc(key);
-     glutSpecialFunc(walk);
+    glutSpecialFunc(walk);
     glutIdleFunc(idle);
 
     glClearColor(1,1,1,1);
